2765265a1da1f3761e3e2d8f27fd81a5b9118ef6
Scott Douglas <git@scottdouglas.net>
2015-07-19 23:16:35 -0400

    First pass of face and upper body detection based on sample code from the OpenCV site.

--- imposture/Cam.cpp
+++ imposture/Cam.cpp
@@ -1,28 +1,105 @@
 #include "Cam.h"
 
-#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <stdio.h>
+
+#include "opencv2/opencv.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/objdetect/objdetect.hpp"
+
+
+using namespace std;
+using namespace cv;
+
+
+static void detectAndDisplay(
+    Mat frame,
+    CascadeClassifier& face_cascade,
+    CascadeClassifier& upperbody_cascade
+) {
+    std::vector<Rect> faces;
+    std::vector<Rect> upperbodies;
+
+    Mat frame_gray;
+    
+    cvtColor(frame, frame_gray, CV_BGR2GRAY);
+    equalizeHist(frame_gray, frame_gray);
+    
+    // Detect faces
+    face_cascade.detectMultiScale(frame_gray, faces, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, Size(30, 30));
+    
+    for(size_t i = 0; i < faces.size(); i++) {
+        Point center(
+            faces[i].x + faces[i].width * 0.5,
+            faces[i].y + faces[i].height * 0.5
+        );
+
+        ellipse(
+            frame, center, Size(faces[i].width * 0.5, faces[i].height * 0.5),
+            0, 0, 360, Scalar(255, 0, 255), 4, 8, 0
+        );
+    }
+    
+    // Detect upper body
+    upperbody_cascade.detectMultiScale(frame_gray, upperbodies);
+    
+    for(size_t i = 0; i < upperbodies.size(); i++) {
+        rectangle(
+            frame,
+            upperbodies[i].tl(),
+            upperbodies[i].br(),
+            Scalar(255,255,255)
+        );
+    }
+    
+    // Show the frame
+    imshow("imposture", frame);
+}
 
 
 int Cam::showCam(){
-    cv::VideoCapture capture(0); // open default camera
+    CascadeClassifier face_cascade;
+    CascadeClassifier upperbody_cascade;
+    
+    // 1. Load the cascades
 
-    if (!capture.isOpened()) {
+    String face_cascade_name = "/data/haarcascade_frontalface_alt.xml";
+    String upperbody_cascade_name = "/data/haarcascade_upperbody.xml";
+    
+    if(!face_cascade.load( face_cascade_name)) {
+        printf("ERROR: Unable to load face cascade.\n");
         return -1;
-    }
+    };
+    if(!upperbody_cascade.load( upperbody_cascade_name)) {
+        printf("ERROR: Unable to load upper body cascade.\n");
+        return -1;
+    };
+    
+    // 2. Read the video stream
 
-    cv::namedWindow("imposture", 1);
+    CvCapture* capture = cvCaptureFromCAM(-1);
 
-    cv::Mat frame;
+    if(capture) {
+        Mat frame;
 
-    while (true) {
-        capture >> frame;
-
-        cv::imshow("imposture", frame);
-
-        int key = cv::waitKey(1);
-        if (key == 27) {
-            break;
+        while (true) {
+            frame = cvQueryFrame(capture);
+            
+            // 3. Apply the classifier to the frame
+            if(!frame.empty()) {
+                detectAndDisplay(frame, face_cascade, upperbody_cascade);
+            } else {
+                printf("WARNING: No captured frame.");
+                break;
+            }
+            
+            int c = waitKey(10);
+            if ((char) c == 'c') {
+                break;
+            }
         }
     }
+
     return 0;
 }
